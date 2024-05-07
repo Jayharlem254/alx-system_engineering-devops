2a35f4f560d1d37153ac2b4ecd81fd3f104356fa
Jayharlem254 <jothamkilimo321@gmail.com>
2024-05-07 12:16:11 +0000

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,29 +3,29 @@
 
 
 def number_of_subscribers(subreddit):
-""" Returns subscriber count of subreddit or 0 """
-from requests import get
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
 
-url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {'user-agent': 'my-app/0.0.1'}
 
-r = get(url, headers=headers, allow_redirects=False)
+    r = get(url, headers=headers, allow_redirects=False)
 
-if r.status_code != 200:
-return 0
+    if r.status_code != 200:
+        return 0
 
-try:
-js = r.json()
+    try:
+        js = r.json()
 
-except ValueError:
-return 0
+    except ValueError:
+        return 0
 
-data = js.get("data")
+    data = js.get("data")
 
-if data:
-sub_count = data.get("subscribers")
-if sub_count:
-return sub_count
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
 
-return 0
+    return 0
